633ee96e305b0d0c4a68669cd870eb0fe76b9809
Paulo Normando <paulonormando@gmail.com>
2022-08-11 13:46:38 -0300

    Refractor SolutionsFragment.kt, SolutionsViewModel.kt

--- app/src/main/java/br/com/imerys/imeryscomercialcatalog/ui/home/SolutionsFragment.kt
+++ app/src/main/java/br/com/imerys/imeryscomercialcatalog/ui/home/SolutionsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import br.com.imerys.imeryscomercialcatalog.databinding.FragmentHomeBinding
+import br.com.imerys.imeryscomercialcatalog.databinding.FragmentSolutionsBinding
 
-class HomeFragment : Fragment() {
+class SolutionsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentSolutionsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val solutionsViewModel =
+            ViewModelProvider(this).get(SolutionsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentSolutionsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        solutionsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
