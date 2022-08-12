941483718c9804537c883bd798b213b9a418866e
Paulo Normando <paulonormando@gmail.com>
2022-08-12 08:12:02 -0300

    Success Cases ui refatored

--- app/src/main/java/br/com/imerys/imeryscomercialcatalog/ui/gallery/SuccessCasesFragment.kt
+++ app/src/main/java/br/com/imerys/imeryscomercialcatalog/ui/gallery/SuccessCasesFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import br.com.imerys.imeryscomercialcatalog.databinding.FragmentGalleryBinding
+import br.com.imerys.imeryscomercialcatalog.databinding.FragmentSuccesscasesBinding
 
-class GalleryFragment : Fragment() {
+class SuccessCasesFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentSuccesscasesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val successCasesViewModel =
+            ViewModelProvider(this).get(SuccessCasesViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentSuccesscasesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        successCasesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
